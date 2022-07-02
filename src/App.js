ae5d3e712ee4bf0fb529cb731527b2c9b965641e
Hendrik Mans <hendrik@mans.de>
2022-07-02 21:07:55 +0200

    Thingy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
+function Thingy() {
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshStandardMaterial color="red" />
+    </mesh>
+  );
+}
+
 export default function App() {
   return (
     <Canvas>
       <Environment preset="studio" background />
       <OrbitControls />
 
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+      <Thingy />
     </Canvas>
   );
 }
