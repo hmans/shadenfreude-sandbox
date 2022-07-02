46faaad731bd9b576cb80c0af8728ae2d6e29f55
Hendrik Mans <hendrik@mans.de>
2022-07-02 21:07:16 +0200

    Eh

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function App() {
-  return <p>hi</p>;
+  return (
+    <Canvas>
+      <Environment preset="studio" background />
+      <OrbitControls />
+
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </Canvas>
+  );
 }
